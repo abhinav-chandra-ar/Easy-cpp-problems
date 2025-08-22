728ff48c50a6c43202e94b28660b599d86ba4564
Abhinav Chandra A R <131592410+abhinav-chandra-ar@users.noreply.github.com>
2025-08-22 22:02:57 +0530

    Add files via upload

---
+++ print_name.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char name[50];
+    
+    //input name
+    cout<< "Enter your name :";
+    cin.getline(name,50) ;
+    
+    //print name
+    cout << "Namah Shivayah! " << name << endl;
+    return  0;
+}	
