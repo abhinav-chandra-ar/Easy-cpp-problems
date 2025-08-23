8ec0356c648cc97cbd85a576ab5cf927e22cefe4
Abhinav Chandra A R <131592410+abhinav-chandra-ar@users.noreply.github.com>
2025-08-23 22:47:14 +0530

    Add files via upload

---
+++ ASCII_values.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char letter;
+	//input letter
+	cout <<"Enter your character :";
+	cin >> letter;
+	
+	//calcultaion and output
+	cout << "ASCII value of " << letter << " is " << int(letter) << endl;
+	
+	return 0;
+}
