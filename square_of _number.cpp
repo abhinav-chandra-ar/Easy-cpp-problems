486d1e0b2397145dc860e51de6204c351fcb23d4
Abhinav Chandra A R <131592410+abhinav-chandra-ar@users.noreply.github.com>
2025-08-23 22:16:39 +0530

    Add files via upload

---
+++ square_of _number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num,result;
+	
+	//Input number
+	cout << "Enter your number :";
+	cin >> num;
+	
+	//square calculation
+	result=num*num;
+	
+	//out the result
+	cout << "The square of your number is : " << result << endl;
+	return 0;
+
+}
