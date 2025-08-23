c2c68820ea1a265a14e355d919f796c0e931fda0
Abhinav Chandra A R <131592410+abhinav-chandra-ar@users.noreply.github.com>
2025-08-23 22:32:33 +0530

    Add files via upload

---
+++ circumference_of_circle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int radius = 5;
+    float pi = 3.1415;
+    
+    // calculation
+    float circumference = 2*pi*radius;
+
+    // output the result
+    cout << "The circumference of your circle is: " << circumference << endl;
+    return 0;
+}
+
