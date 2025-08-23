5ddc6d7cfae7e44715761f35e7acad6364bb7e14
Abhinav Chandra A R <131592410+abhinav-chandra-ar@users.noreply.github.com>
2025-08-23 22:32:18 +0530

    Add files via upload

---
+++ area_of_circle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int radius = 5;
+    float pi = 3.1415;
+    
+    // calculation
+    float area = pi*radius*radius;
+
+    // output the result
+    cout << "The area of your circle is: " << area << endl;
+    return 0;
+}
+
